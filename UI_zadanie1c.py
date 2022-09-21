50b040302ac121b026d2f40f02d699da8c1af129
JakubGasparin <55839847+JakubGasparin@users.noreply.github.com>
2022-09-21 16:56:38 +0200

    Add files via upload

--- UI_zadanie1c.py
+++ UI_zadanie1c.py
@@ -1,30 +1,34 @@
 from array import array
 from copy import deepcopy
 
-
-class Hlavolam:
-    def __init__(self, root):
-        pass
-
 class Node:
-    def __init__(self, parent, array, size_row, size_col):
+    def __init__(self, parent, array):
         self.parent=parent
         self.left=None
         self.right=None
         self.down=None
         self.up=None
         self.array=array
-        self.size_row=size_row
-        self.size_col=size_col
+        self.size_row=len(array)
+        self.size_col=len(array[0])
 
+
+class Hlavolam:
+
+    def __init__(self, root):
+        self.root=root
+        self.size_row=len(root.array)
+        self.size_col=len(root.array[0])
 
     def _search(self):
-        print(self.array, self.size_row, self.size_col)
+        #print(self.array, self.size_row, self.size_col)
+        current_node = self.root
+        print(current_node.array)
         row=0
         col=0
         for i in range(self.size_row):
             for j in range(self.size_col):
-                if self.array[i][j]=='m':
+                if current_node.array[i][j]=='m':
                     #print("found m")
                     #print(i,j)
                     row=i
@@ -35,31 +39,31 @@ class Node:
         #idem HORE 
         if row-1 >=0:
             print("swapping up...")
-            self._swap(row-1, col, row, col)
+            current_node.down=Node(current_node,self._swap(row-1, col, row, col, current_node.array))
         
         #idem DOLE
         if row+1 < self.size_row:
             print("swapping down...")
-            self._swap(row+1, col, row, col)
+            current_node.down=Node(current_node,self._swap(row+1, col, row, col, current_node.array))
 
         #idem DOLAVA
         if col-1 >=0:
             print("swapping left...")
-            self._swap(row, col-1, row, col)
+            current_node.left=Node(current_node,self._swap(row, col-1, row, col, current_node.array))
         else: 
             print("cant move left")
 
         #idem DOPRAVA 
         if col+1 < self.size_row:
             print("swapping right...")
-            self._swap(row, col+1, row, col)
+            current_node.right=Node(current_node,self._swap(row, col+1, row, col, current_node.array))
         else: 
             print("cant move right")
         
         
     
-    def _swap(self, row, col, row_m, col_m):
-        temp_array = deepcopy(self.array)
+    def _swap(self, row, col, row_m, col_m, array):
+        temp_array = deepcopy(array)
         temp=temp_array[row][col]
         #print(temp, row, col, row_m, col_m)
         temp_array[row][col] = temp_array[row_m][col_m]
@@ -83,8 +87,9 @@ if __name__ == "__main__":
             col.append(value)
         arr.append(col)
     #print(arr)
-    t=Node(None,arr, rows, cols)
-    t._search()
+    root=Node(None,arr)
+    hlavolam = Hlavolam(root)
+    hlavolam._search()
 
 
 
